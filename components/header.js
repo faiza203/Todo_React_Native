f4ab4604c006891c8fcc4fa3135784db5f3ee622
faiza203 <fiazfaiza981@gmail.com>
2021-02-04 10:58:55 -0500

    Style the Header

--- components/header.js
+++ components/header.js
@@ -2,5 +2,23 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
 export default function Header() {
-  return <Text>HEADER</Text>;
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My Todos</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "coral",
+  },
+  title: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
